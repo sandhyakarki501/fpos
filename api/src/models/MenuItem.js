2d1acff584186060effe17ac8d8bb23463f6abe4
samarajya <sandhyakarki501@gmail.com>
2025-03-09 13:29:20 +0545

    feat: update menu model

--- api/src/models/MenuItem.js
+++ api/src/models/MenuItem.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
 const menuItemSchema = new mongoose.Schema({
-  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  category: { type: String },
   description: { type: String },
-  imageUrls: { type: [String] },
+  imageUrls: { type: [String], required: false },
   isActive: { type: Boolean, default: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  stockQuantity: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now() },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 export default mongoose.model("MenuItem", menuItemSchema);
