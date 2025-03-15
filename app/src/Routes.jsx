3c7c03687680817c8ad8ba9193f7986a109b8eba
samarajya <sandhyakarki501@gmail.com>
2025-03-15 17:31:04 +0545

    feat: create cart page

--- app/src/Routes.jsx
+++ app/src/Routes.jsx
@@ -7,21 +7,23 @@ import {
 import Home from "./pages/Home";
 import {
   ADD_MENU_ITEM_ROUTE,
+  CART_ITEMS_ROUTE,
   EDIT_MENU_ITEM_ROUTE,
   LIST_MENU_ITEM_ROUTE,
   LOGIN_ROUTE,
   MENU_ITEMS_ROUTE,
   REGISTER_ROUTE,
 } from "./constants/routes";
-import MainLayout from "./layouts/MainLayout";
+import AddMenuItem from "./pages/menuItems/Add";
+import AuthLayout from "./layouts/AuthLayout";
+import CartItems from "./pages/cart";
+import EditMenuItem from "./pages/menuItems/Edit";
 import Login from "./pages/auth/Login";
+import MainLayout from "./layouts/MainLayout";
+import MenuItems from "./pages/menuItems";
+import MenuItemsList from "./pages/menuItems/List";
 import Register from "./pages/auth/Register";
 import UnAuthLayout from "./layouts/UnAuthLayout";
-import AuthLayout from "./layouts/AuthLayout";
-import MenuItems from "./pages/menuItems";
-import AddMenuItem from "./pages/menuItems/Add";
-import EditMenuItem from "./pages/menuItems/Edit";
-import MenuItemsList from "./pages/menuItems/List";
 
 const Routes = () => {
   const router = createBrowserRouter(
@@ -31,6 +33,7 @@ const Routes = () => {
         <Route path={MENU_ITEMS_ROUTE} element={<MenuItems />} />
 
         <Route element={<AuthLayout />}>
+          <Route path={CART_ITEMS_ROUTE} element={<CartItems />} />
           <Route path={LIST_MENU_ITEM_ROUTE} element={<MenuItemsList />} />
           <Route path={ADD_MENU_ITEM_ROUTE} element={<AddMenuItem />} />
           <Route
